6b59a387c3e4084b98f07eb892e7a1a2849a3cf3
optionaltoast <robin.b.martin@gmail.com>
2021-08-20 16:11:36 -0600

    zippo: decommonize sensors
    Change-Id: Iad917c6d9026ded7ba5c28e7e4f9fff604232c13

--- device.mk
+++ device.mk
@@ -4,10 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
-
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths_tavil.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tavil.xml \
@@ -24,6 +20,11 @@ TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_FACE_UNLOCK := true
 PRODUCT_PACKAGES += \
     FaceUnlockService
+
+# Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@2.0-service.multihal \
+    android.hardware.sensors@2.0-ScopedWakelock.vendor
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
