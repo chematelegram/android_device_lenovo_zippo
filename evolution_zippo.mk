9695fdedf1e74afc1df7c21a6cf6b59ec649e4f5
chematelegram <chfondia@gmail.com>
2021-09-23 00:47:45 +0200

    zippo: Don't build FP nor override PRIVATE_BUILD_DESC

--- evolution_zippo.mk
+++ evolution_zippo.mk
@@ -20,10 +20,3 @@ PRODUCT_MODEL := Lenovo L78051
 PRODUCT_NAME := evolution_zippo
 
 PRODUCT_GMS_CLIENTID_BASE := android-lenovo
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210805.001.A1 7474174 release-keys" \
-    PRODUCT_NAME="zippo" \
-    TARGET_DEVICE="zippo"
-
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys"
