09f7232edb3762844c7322453deaefc7b98a0831
Akash Srivastava <akashniki@gmail.com>
2022-02-01 16:22:15 +0100

    zippo: Enable quick-tap

--- evolution_zippo.mk
+++ evolution_zippo.mk
@@ -9,7 +9,7 @@ $(call inherit-product, device/lenovo/zippo/device.mk)
 # Inherit some common EvolutionX stuff.
 EXTRA_UDFPS_ANIMATIONS := true
 TARGET_BOOT_ANIMATION_RES := 1080
-WITH_GAPPS := true
+TARGET_SUPPORTS_QUICK_TAP := true
 $(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
