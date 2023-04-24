abc787dfe56b1c4f6ec19f2516f641fe4cbaf22c
chematelegram <chfondia@gmail.com>
2023-04-24 16:36:08 +0000

    zippo: Shishufy the Zippo

--- bootleg_zippo.mk
+++ bootleg_zippo.mk
@@ -8,6 +8,8 @@ $(call inherit-product, device/lenovo/zippo/device.mk)
 
 # Inherit some common Bootleggers stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
+DEVICE_MAINTAINERS := Chema_Funtan
+BOOTLEGGERS_BUILD_TYPE := Shishufied
 $(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
