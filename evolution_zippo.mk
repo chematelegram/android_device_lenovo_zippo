e64def4887b87f87ee0e0ae0385913547be392bc
chematelegram <chfondia@gmail.com>
2023-08-09 05:29:09 +0000

    zippo: Build Aperture camera

--- evolution_zippo.mk
+++ evolution_zippo.mk
@@ -10,6 +10,7 @@ $(call inherit-product, device/lenovo/zippo/device.mk)
 EVO_BUILD_TYPE := OFFICIAL
 EXTRA_UDFPS_ANIMATIONS := true
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_BUILD_APERTURE_CAMERA := true
 TARGET_SUPPORTS_QUICK_TAP := true
 $(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
